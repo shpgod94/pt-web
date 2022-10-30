27205f3270f5c8f9a9c1ead472f8d45557003ef8
eric <shpgod94@naver.com>
2022-10-30 21:04:14 +0900

    first commit

---
+++ src/main/java/com/pt/manage/ptweb/PtWebApplication.java
@@ -0,0 +1,13 @@
+package com.pt.manage.ptweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtWebApplication.class, args);
+	}
+
+}
